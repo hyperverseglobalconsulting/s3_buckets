73d0c9160b4b707490e9fea8b3581ec47b33f12d
hyperverseglobalconsulting <126176088+hyperverseglobalconsulting@users.noreply.github.com>
2023-02-23 21:53:00 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,25 +3,25 @@ data "aws_s3_bucket" "datavid_pdfconverter" {
 }
 
 resource "aws_s3_bucket_object" "project_data_masked_images" {
-  bucket = aws_s3_bucket.datavid_pdfconverter.id
+  bucket = data.aws_s3_bucket.datavid_pdfconverter.id
   key    = var.key_prefix_masked_images
   acl    = "private"
 }
 
 resource "aws_s3_bucket_object" "project_data_opensearch_data" {
-  bucket = aws_s3_bucket.datavid_pdfconverter.id
+  bucket = data.aws_s3_bucket.datavid_pdfconverter.id
   key    = var.key_prefix_opensearch_data
   acl    = "private"
 }
 
 resource "aws_s3_bucket_object" "project_data_table_corners" {
-  bucket = aws_s3_bucket.datavid_pdfconverter.id
+  bucket = data.aws_s3_bucket.datavid_pdfconverter.id
   key    = var.key_prefix_table_corners
   acl    = "private"
 }
 
 resource "aws_s3_bucket_object" "project_data_bbox_images" {
-  bucket = aws_s3_bucket.datavid_pdfconverter.id
+  bucket = data.aws_s3_bucket.datavid_pdfconverter.id
   key    = var.key_prefix_bbox_images
   acl    = "private"
 }
