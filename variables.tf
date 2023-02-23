df82b3e1854ec88f82f7f6eda55037c7c303b540
hyperverseglobalconsulting <126176088+hyperverseglobalconsulting@users.noreply.github.com>
2023-02-23 20:52:05 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,17 +8,17 @@ variable "key_prefix_masked_images" {
     default = "project/data/masked_images/"
 }
 
-resource "key_prefix_opensearch_data" {
+variable "key_prefix_opensearch_data" {
     type     = string 
     default = "project/data/opensearch_data/"
 }
 
-resource "key_prefix_table_corners" {
+variable "key_prefix_table_corners" {
     type    = string
     default = "project/data/table_corners/"
 }
 
-resource "key_prefix_bbox_images" {
+variable "key_prefix_bbox_images" {
     type    = string
     default = "project/data/bbox_images/"
 }
