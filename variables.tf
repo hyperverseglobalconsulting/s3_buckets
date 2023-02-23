4ff6bd7dfc2ea7f4483eb0b2a654cf400dcce1f0
hyperverseglobalconsulting <126176088+hyperverseglobalconsulting@users.noreply.github.com>
2023-02-23 20:47:43 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,25 +4,21 @@ variable "bucket_name" {
 }
 
 variable "key_prefix_masked_images" {
-  bucket = aws_s3_bucket.datavid_pdfconverter.id
-  key    = "project/data/masked_images/"
-  acl    = "private"
+    type    = string
+    default = "project/data/masked_images/"
 }
 
 resource "key_prefix_opensearch_data" {
-  bucket = aws_s3_bucket.datavid_pdfconverter.id
-  key    = "project/data/opensearch_data/"
-  acl    = "private"
+    type     = string 
+    default = "project/data/opensearch_data/"
 }
 
 resource "key_prefix_table_corners" {
-  bucket = aws_s3_bucket.datavid_pdfconverter.id
-  key    = "project/data/table_corners/"
-  acl    = "private"
+    type    = string
+    default = "project/data/table_corners/"
 }
 
 resource "key_prefix_bbox_images" {
-  bucket = aws_s3_bucket.datavid_pdfconverter.id
-  key    = "project/data/bbox_images/"
-  acl    = "private"
+    type    = string
+    default = "project/data/bbox_images/"
 }
